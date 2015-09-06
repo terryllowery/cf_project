f31a16e130af907be6b3bfd22d9419ad74caaaaf
Terry Lowery <terry@lowerytech.net>
2015-09-05 23:33:53 -0500

    Exercise_17

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
     $(".message-box").css("background-color", "pink");
+    $("#form-contact-message").on("keyup", function(){
+        var charCount = $("#form-contact-message").val().length;
+        console.log(charCount);
+        $("#char-count").html(charCount);
+
+        if (charCount > 50 ) {
+            $("#char-count").css("color", "red");
+            console.log("charCount is greater than 50");
+        }
+        else {
+            $("#char-count").css("color", "white");
+            console.log("charCount is currently: " + charCount);
+        }
+
+    });
     $("#submit-button").on("click", function() {
         var message = $(".message-box").val();
         console.log("clicked!");
