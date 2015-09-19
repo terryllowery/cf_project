9832a7068d121d8f9367dd2a8b3f9fe6d140be99
Terry Lowery <terry@lowerytech.net>
2015-09-19 17:45:05 -0500

    Exercise_20

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ var myKitties = [ { title: "First Project", pic: "img/coming-soon.jpg"},
     { title: "Fourth Project", pic: "img/coming-soon.jpg"}
 ];
 
+
 $(document).ready(function() {
     $(".message-box").css("background-color", "pink");
     $("#form-contact-message").on("keyup", function(){
@@ -36,33 +37,44 @@ $(document).ready(function() {
         $("#visible-comment").css("color", "red");
         return false;
     });
-
-
     for (var i = 0; i < myKitties.length; ++i ) {
-        console.log("i is: " + i);
-        console.log("url = " + myKitties[i].pic);
-        console.log("id = " +  myKitties[i].title );
+        console.info("Iterating through images array.")
+        console.info("i is: " + i);
+        console.info("url = " + myKitties[i].pic);
+        console.info("id = " +  myKitties[i].title );
         $("#" + i).css("background-image", "url(" + myKitties[i].pic + ")");
 
     }
-
     $(".image").mouseenter( function() {
         console.info(this);
-        console.log(myKitties[this.id].title);
         $(this).html("<p class='info'><span class='proj-title'>Title:</span> " + myKitties[this.id].title + "</p> ");
         $("p.info").css("color", "rgb(255, 0, 0");
     }).mouseleave( function() {
         $('p.info').html("");
     });
-
+// Intialize google maps
     function initialize() {
         var mapOptions = {
-            center: new google.maps.LatLng(52.4918598,13.427565,18),
+            center: new google.maps.LatLng(29.4262315,-98.4934736),
             zoom: 8
         };
-    var map = new google.maps.map(document.getElementById("maps-canas"), mapOptions);
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    console.info("Intialized google maps");
 
+        var marker = new google.maps.Marker( {
+            position: map.getCenter(),
+            map: map,
+            title: 'Click to center',
+            animation: google.maps.Animation.BOUNCE
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+            map.setZoom(18);
+            map.setCenter(marker.getPosition());
+            console.info("Set new position for maps: " + marker.getPosition());
+        })
 
-    };
+    }
     google.maps.event.addDomListener(window, 'load', initialize);
+
+
 });
